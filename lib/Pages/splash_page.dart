36a7c83cfd516804eb9e2e85f79b5ccc63a045ea
Adit Swami <aditswami@Adits-MBP.lan>
2024-10-04 02:18:19 -0500

    finished splash page UI

--- lib/Pages/splash_page.dart
+++ lib/Pages/splash_page.dart
@@ -9,6 +9,7 @@ class SplashPage extends StatelessWidget {
     return Container(
         color: const Color.fromRGBO(185, 209, 234, 1),
         child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          //floating page dark blue
           Padding(
             padding: const EdgeInsets.only(top: 80.0),
             child: Container(
@@ -25,10 +26,90 @@ class SplashPage extends StatelessWidget {
                         offset: Offset(3, 0))
                   ]),
               child: Column(
-                children: [Container()],
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Explore the app',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 15),
+                    child: Text(
+                      'All your health needs in one place',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  //sign in options
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      width: 353,
+                      height: 54,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(28),
+                          color: const Color.fromRGBO(24, 90, 135, 100)),
+                      child: const Align(
+                          alignment: Alignment(-0.3, 0),
+                          child: Text(
+                            'Sign in with Google',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          )),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      width: 353,
+                      height: 54,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(28),
+                          color: const Color.fromRGBO(24, 90, 135, 100)),
+                      child: const Align(
+                          alignment: Alignment(-0.3, 0),
+                          child: Text(
+                            'Sign in with Apple',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          )),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      width: 353,
+                      height: 54,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(28),
+                          color: const Color.fromRGBO(24, 90, 135, 100)),
+                      child: const Align(
+                          alignment: Alignment(-0.3, 0),
+                          child: Text(
+                            'Continue with Email',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          )),
+                    ),
+                  )
+                ],
               ),
             ),
           ),
+          const Padding(
+            padding: const EdgeInsets.all(60.0),
+            child: Text('Already have an account? Log in'),
+          )
         ]));
   }
 }
