3d9d3952e3570ef4ef480e2224a85c0b30a7dae9
Adit Swami <aditswami@Adits-MacBook-Pro.local>
2024-10-18 15:05:40 -0500

    AI works now, but no UI

--- lib/Pages/chatbot_page.dart
+++ lib/Pages/chatbot_page.dart
@@ -1,14 +1,21 @@
 import 'dart:io';
 
+import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 
-class Chatbot extends StatelessWidget {
+class Chatbot extends StatefulWidget {
   Chatbot({super.key});
 
+  @override
+  State<Chatbot> createState() => _ChatbotState();
+}
+
+class _ChatbotState extends State<Chatbot> {
   // gemini initialization
   final apiKey =
       Platform.environment['AIzaSyAI_pfX1CmlHRgaLKWI8_cfEtQyt9Ngpcs'];
+
   final gemini = GenerativeModel(
     model: 'gemini-1.5-pro-002',
     apiKey: 'AIzaSyAI_pfX1CmlHRgaLKWI8_cfEtQyt9Ngpcs',
@@ -16,13 +23,17 @@ class Chatbot extends StatelessWidget {
         'You are a doctor that will diagnose a patient\'s sickness or injury based on their inputted symptoms. Accuracy is crucial and keep responses brief.'),
   );
 
+  ChatUser currentUser = ChatUser(id: '0', firstName: 'You');
+
+  ChatUser geminiUser = ChatUser(id: '1', firstName: 'Gemini');
+
+  List<ChatMessage> messages = [];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 393,
-      height: 852,
-      clipBehavior: Clip.antiAlias,
-      decoration: const BoxDecoration(color: Color(0xFFB9D1EA)),
+    return Scaffold(
+      backgroundColor: Color(0xFFB9D1EA),
+      body: _builUI(),
     );
   }
 
@@ -30,5 +41,41 @@ class Chatbot extends StatelessWidget {
     GenerateContentResponse response =
         await gemini.generateContent([Content.text(input)]);
     return response.text;
+  }
+
+  Widget _builUI() {
+    return DashChat(
+        currentUser: currentUser, onSend: _sendMessage, messages: messages);
+  }
+
+  void _sendMessage(ChatMessage chatMessage) {
+    setState(() {
+      messages = [chatMessage, ...messages];
+    });
+
+    try {
+      String question = chatMessage.text;
+
+      // Start listening to the response from Gemini.
+      gemini.generateContentStream([Content.text(question)]).listen(
+          (GenerateContentResponse event) {
+        // Get the response text from Gemini.
+        String responseText = (event.text).toString();
+
+        // Create a new ChatMessage for Gemini's response.
+        ChatMessage geminiResponse = ChatMessage(
+          user: geminiUser,
+          text: responseText,
+          createdAt: DateTime.now(),
+        );
+
+        // Update the UI with Gemini's response.
+        setState(() {
+          messages = [geminiResponse, ...messages];
+        });
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 }
--- lib/Pages/registration_page.dart
+++ lib/Pages/registration_page.dart
@@ -4,6 +4,7 @@ import 'package:email_validator/email_validator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:cac_med_app/components/textfield.dart';
 
 class Registration extends StatefulWidget {
   const Registration({super.key});
@@ -63,13 +64,13 @@ class _RegistrationState extends State<Registration> {
               ),
             ),
             const SizedBox(height: 25),
-            MyTextField(
+            MyCupertinoTextField(
               controller: emailController,
               hintText: 'Email',
               obscureText: false,
             ),
             const SizedBox(height: 30),
-            MyTextField(
+            MyCupertinoTextField(
               controller: passwordController,
               hintText: 'Password',
               obscureText: true,
